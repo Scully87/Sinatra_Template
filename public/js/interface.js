5244a66d8797ce8e0210aa0b73f551edcff4cee7
Scully87 <scully87.ts@googlemail.com>
2014-12-15 11:03:31 +0000

    changed description

--- public/js/interface.js
+++ public/js/interface.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-  var text = 'Thank you for downloading the "Sinatra_Template" Gem, As you may have noticed CSS, Normalize, Jquery and an Image folder have all been required so that you are ready to go build this page quickly and easily. Simply start editing from "index.erb" and the related files. *** Remember to remove the ".git" file and update the "readme.md" if you are going to create your own repository from this template.***\n\nENJOY! :-)';
+  var text = 'Thank you for downloading the "Sinatra_Template" Gem, As you may have noticed CSS, Normalize and Jquery have all been required so that you are ready to go build this page quickly and easily. Simply start editing from "index.erb" and the related files. *** Remember to remove the ".git" file and update the "readme.md" if you are going to create your own repository from this template.***\n\nENJOY! :-)';
             $('button').click(function(){
                 var span = $('<span/>').insertAfter(this);
                 $(this).remove();
